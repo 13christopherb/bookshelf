6d5491e620ea09e0c2284525a9998e2660db75dd
Chris Berglund <13christopherb@gmail.com>
2017-10-26 13:07:48 -0700

    feat: Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BooksApp extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return(
+        <div>Hello</div>
+        )
+    }
+}
+
+
+
+export default BooksApp;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BooksApp from './App';
+
+ReactDOM.render(<BooksApp />, document.getElementById('root'));
